75bb71f9b566095dcf7e90285afe48d6691d3a67
Kazumasa Shimomura <you@yourdomain.com>
2016-12-30 21:49:55 +0900

    Tabレイアウト実装（切り替え時に遷移はできない）

--- app/src/main/java/com/example/satoken/myscheduler/MainActivity.java
+++ app/src/main/java/com/example/satoken/myscheduler/MainActivity.java
@@ -1,21 +1,37 @@
 package com.example.satoken.myscheduler;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import io.realm.Realm;
 import io.realm.RealmResults;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener,
+        PageFragment.OnFragmentInteractionListener {
 
     ListView mListView;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +39,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // xmlからTabLayoutの取得
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        // xmlからViewPagerを取得
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        // ページタイトル配列
+        final String[] pageTitle = {"HOME", "EVENT"};
+
+        // 表示Pageに必要な項目を設定
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return PageFragment.newInstance(position + 1);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return pageTitle[position];
+            }
+
+            @Override
+            public int getCount() {
+                return pageTitle.length;
+            }
+        };
+
+        // ViewPagerにページを設定
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(this);
+
+        // ViewPagerをTabLayoutを設定
+        tabLayout.setupWithViewPager(viewPager);
 
         mListView = (ListView) findViewById(R.id.listView);
 
@@ -38,9 +86,8 @@ public class MainActivity extends AppCompatActivity {
         Realm realm = Realm.getInstance(this);
         RealmResults<Schedule> schedules =
                 realm.where(Schedule.class).findAll();
-        ScheduleAdapter adapter =
-                new ScheduleAdapter(this, schedules, true);
-        mListView.setAdapter(adapter);
+        ScheduleAdapter scAdapter = new ScheduleAdapter(this, schedules, true);
+        mListView.setAdapter(scAdapter);
         findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,10 +110,65 @@ public class MainActivity extends AppCompatActivity {
                           idをScheduleEditActivityに渡している */
                         startActivity(new Intent(MainActivity.this,
                                 ScheduleEditActivity.class)
-                        .putExtra("schedule_id", schedule.getId()));
+                                .putExtra("schedule_id", schedule.getId()));
 
                     }
                 }
         );
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
     }
 }
